5115320561f62df19fae2a2d7f6aaa065129010c
Juan in a million <59286923+damianjuan@users.noreply.github.com>
2020-03-29 18:02:53 -0500

    added pseudocode

--- Unsolved/script.js
+++ Unsolved/script.js
@@ -1,6 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// prompt for user password requirements
+
+
+
+//generate password
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +20,15 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+// pseudocode
+// prompt user to enter length of password
+// prompt user if capital required
+// prompt if number is required  
+// prompt user if special characters are required
+// generate password
+// check to make sure one of each user requirement is include in password if not generate password again
+// write password  
